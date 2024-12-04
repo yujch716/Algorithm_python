ada701813c3ce9f6c0586bce5e76c6f5e829b99e
stead-yujin <70509403+yujch716@users.noreply.github.com>
2024-12-05 01:21:32 +0900

    템플릿 저장

---
+++ template.py
@@ -0,0 +1,19 @@
+# 문제 제목 :
+# 난이도 :
+# 링크 :
+
+def solution(num1):
+    return
+
+
+# 테스트 코드
+if __name__ == "__main__":
+    test_cases = [
+
+    ]
+
+    for inputs, expected in test_cases:
+        num1 = inputs
+        result = solution(num1)
+        assert result == expected, f"실패: 입력값={inputs}, 기대값={expected}, 결과값={result}"
+    print("성공!")
