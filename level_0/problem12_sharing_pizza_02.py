d68dfca220d298324e2459f31dcd5d9cd6d8ad6b
stead-yujin <70509403+yujch716@users.noreply.github.com>
2024-12-09 05:26:47 +0900

    피자 나눠 먹기(2) 풀이

---
+++ level_0/problem12_sharing_pizza_02.py
@@ -0,0 +1,24 @@
+# 문제 제목 : 피자 나눠 먹기 (2)
+# 난이도 : Level 0
+# 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120815
+
+import math
+
+
+def solution(n):
+    return (n * 6) // math.gcd(n, 6) // 6
+
+
+# 테스트 코드
+if __name__ == "__main__":
+    test_cases = [
+        ((6), 1),
+        ((10), 5),
+        ((4), 2)
+    ]
+
+    for inputs, expected in test_cases:
+        n = inputs
+        result = solution(n)
+        assert result == expected, f"실패: 입력값={inputs}, 기대값={expected}, 결과값={result}"
+    print("성공!")
