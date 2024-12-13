d0c9f496b20f6798dd40ba4c15cd888d787d0da4
stead-yujin <70509403+yujch716@users.noreply.github.com>
2024-12-14 03:56:58 +0900

    대문자와 소문자 풀이

---
+++ level_0/problem13_uppercase_and_lowercase.py
@@ -0,0 +1,21 @@
+# 문제 제목 : 대문자와 소문자
+# 난이도 : Level 0
+# 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120893
+
+def solution(my_string):
+    return my_string.swapcase()
+
+
+# 테스트 코드
+if __name__ == "__main__":
+    test_cases = [
+        (("cccCCC"), "CCCccc"),
+        (("abCdEfghIJ"), "ABcDeFGHij"),
+        (("Hello"), "hELLO")
+    ]
+
+    for inputs, expected in test_cases:
+        my_string = inputs
+        result = solution(my_string)
+        assert result == expected, f"실패: 입력값={inputs}, 기대값={expected}, 결과값={result}"
+    print("성공!")
