97182f70ff75a6d4dd02e72d53c4429c53b233cb
stead-yujin <70509403+yujch716@users.noreply.github.com>
2024-12-16 00:00:47 +0900

    특정한 문자를 대문자로 바꾸기

--- level_0/change_upper.py
+++ level_0/change_upper.py
@@ -3,7 +3,7 @@
 # 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181873
 
 def solution(my_string, alp):
-    return "".join([i.upper() if i == alp else i for i in my_string])
+    return my_string.replace(alp, alp.upper())
 
 
 # 테스트 코드
